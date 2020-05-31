b628e6c65519db40a645fd185704d9eb3f447531
fonzerelly <christian.hoerauf@gmail.com>
2020-05-31 12:08:47 +0200

    implements greet component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
-
+import Greet from './Greet'
 
 
 function App() {
   return (
     <div className="App">
       <h1>Student Grading</h1>
+      <Greet />
     </div>
   )
 }
---
+++ src/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const greet = () => alert ("Hallo Ansbach")
+
+const Greet = () => (<button onClick={greet}>Greet</button>)
+
+export default Greet;
